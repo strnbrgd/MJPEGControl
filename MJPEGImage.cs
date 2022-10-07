986acde943b76f65237bddefd4e718c35bb32852
strnbrgd <91142600+strnbrgd@users.noreply.github.com>
2022-10-07 18:49:13 +0300

    Initial Commit

---
+++ MJPEGImage.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using CommunityToolkit.Maui.Converters;
+
+
+namespace MJPEGControl
+{
+    /// <summary>
+    /// View that shows MJPEG stream
+    /// Uses Maui Toolkit - must add UseMauiCommunityToolkit
+    /// </summary>
+    public class MJPEGImage : Image
+    {
+        private readonly MjpegProcessor.MjpegDecoder Decoder = new();
+
+        #region MJPEGSource Property
+
+        public static readonly BindableProperty MJPEGSourceProperty = BindableProperty.Create("MJPEGSource",
+            typeof(string),
+            typeof(MJPEGImage),propertyChanged:MJPEGSourceChanged);
+
+        public string MJPEGSource
+        {
+            get => (string) GetValue(MJPEGSourceProperty);
+            set => SetValue(MJPEGSourceProperty, value);
+        }
+
+        
+
+        private static void MJPEGSourceChanged(BindableObject Bindable, object OldValue, object NewValue)
+        {
+            if (!(Bindable is MJPEGImage image)) return;
+            var newStreamUri = NewValue as string;
+            if (string.IsNullOrEmpty(newStreamUri))
+                image.Decoder.StopStream();
+            else
+                image.Decoder.ParseStream(new Uri(newStreamUri));
+        }
+
+        #endregion
+
+        private readonly ByteArrayToImageSourceConverter m_converter = new();
+
+        public delegate void StringDelegate(string ErrorMessage);
+
+        public event StringDelegate Error;
+
+        public MJPEGImage()
+        {
+            Decoder.FrameReady += (s, e) => { Source = m_converter.ConvertFrom(e.FrameBuffer, CultureInfo.CurrentCulture);};
+            Decoder.Error += (s,e)=> {Error?.Invoke(e.Message);};
+        }
+
+        
+    }
+}
---
+++ Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MJPEGControl
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
